9fb04acb814eaa94e80e149b7a665324f28bdb2e
mahmudfarsi <mhfarsi002022@gmail.com>
2023-10-22 13:28:49 +0330

    creat script for navBar and add class to navBar

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 const $ = document;
 const anchor = $.querySelectorAll('.anchor');
 const containers = $.querySelectorAll('.container');
+const navList = $.querySelector('.nav');
 let idSection = 'home';
 
 window.addEventListener('scroll',()=>{
-  
   containers.forEach(item=>{
     if(window.scrollY >= item.offsetTop){
       idSection = item.id
     }
   });
-
   anchor.forEach(items=>{
     if(items.href.includes(idSection)){
       items.classList.add('show');
@@ -19,6 +18,19 @@ window.addEventListener('scroll',()=>{
       items.classList.remove('show');
     }
   });
-
+   
+   // for nav
+   if(document.documentElement.scrollTop > 20 || document.scrollTop > 20){
+    navList.classList.add('show');
+  }else{
+    navList.classList.remove('show');
+  }
+ 
 });
+
+
+
+
+
+
 
